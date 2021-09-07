149f1e5af528c93dfa432022e6fefc0b3465c116
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2021-09-07 16:27:50 +0700

    Update global.js

--- penghitung-wr/assets/js/global.js
+++ penghitung-wr/assets/js/global.js
@@ -3,14 +3,14 @@ function welcomeMsg() {
     let array = 0;
 
     const takapedia = `<a href="https://msha.ke/takapedia/" target="_blank">takapedia</a>`;
-    const takapediaIG = `<a href="https://www.instagram.com/taka_pedia/" target="_blank">taka_pedia</a>`;
-    const whatsapp = `<a href="https://api.whatsapp.com/send?phone=6285793432434" target="_blank">takapedia</a>`;
-    const instagram = `<a href="https://vt.tiktok.com/ZSJQ5XuUu/" target="_blank">tiktok</a>`
+    const IgTaka = `<a href="https://www.instagram.com/taka_pedia/" target="_blank">IgTakapedis</a>`;
+    const whatsapp = `<a href="https://api.whatsapp.com/send?phone=6285793432434" target="_blank">WhatsApp</a>`;
+    const instagram = `<a href="https://www.instagram.com/shitpost.id_._/" target="_blank">Instagram</a>`
 
     const msg1 = `<p class="animation mb-0 text-truncate">JOKI, TOP UP & GIFT SKIN ML di ${takapedia} ya</p>`;
-    const msg3 = `<p class="animation mb-0 text-truncate">Takapedia dijamin amanah dan terpercaya</p>`;
+    const msg3 = `<p class="animation mb-0 text-truncate">Takapedia dijamin amanah dan terpercaya ${IgTaka}</p>`;
     const msg4 = `<p class="animation mb-0 text-truncate">Follow ${instagram}</p>`;
-    const msg5 = `<p class="animation mb-0 text-truncate">Contact whatsapp ${whatsapp}</p>`;
+    const msg5 = `<p class="animation mb-0 text-truncate">Contact ${whatsapp}</p>`;
 
     let arrayEl = [msg1, msg3, msg4, msg5];
     let arrayMax = 4;
