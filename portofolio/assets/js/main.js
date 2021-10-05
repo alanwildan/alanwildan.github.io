7c4cf32afb5a2410168615af40ac450acf5a6b9e
AlanX404 <76613101+alanwildan@users.noreply.github.com>
2021-10-05 15:35:51 +0700

    Update main.js

--- portofolio/assets/js/main.js
+++ portofolio/assets/js/main.js
@@ -28,8 +28,8 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
-    distance: '80px',
-    duration: 2000,
+    distance: '100px',
+    duration: 3000,
     reset: true
 });
 
